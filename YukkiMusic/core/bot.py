7bbe49a0cecdb50960b3c98ab5900928031797a5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2024-06-30 19:42:38 +0530

    Update bot.py

--- YukkiMusic/core/bot.py
+++ YukkiMusic/core/bot.py
@@ -16,13 +16,6 @@ import config
 
 from ..logging import LOGGER
 
-self.app2 = Client(
-    "YukkiMusic",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    bot_token=config.BOT_TOKEN2,
-)
-
 class YukkiBot(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot")
@@ -32,27 +25,32 @@ class YukkiBot(Client):
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
         )
-        self.two = app2
+        self.two = Client(
+            "YukkiMusic",
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            bot_token=config.BOT_TOKEN2,
+        )
 
     async def start(self):
-
         await super().start()
         get_me = await self.get_me()
         self.username = get_me.username
         self.id = get_me.id
-        self.name = self.me.first_name + " " + (self.me.last_name or "")
-        self.mention = self.me.mention
+        self.name = get_me.first_name + " " + (get_me.last_name or "")
+        self.mention = get_me.mention
 
         try:
             await self.send_message(
                 config.LOG_GROUP_ID,
                 text=f"<u><b>{self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
             )
-        except:
+        except Exception as e:
             LOGGER(__name__).error(
-                "Bot has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                f"Bot has failed to access the log Group. Error: {str(e)}"
             )
             sys.exit()
+
         if config.SET_CMDS:
             try:
                 await self.set_bot_commands(
@@ -65,87 +63,68 @@ class YukkiBot(Client):
                         BotCommand("resume", "ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴘᴀᴜsᴇᴅ sᴏɴɢ"),
                         BotCommand("end", "ᴄʟᴇᴀʀ ᴛʜᴇ ǫᴜᴇᴜᴇ ᴀᴍᴅ ʟᴇᴀᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛ"),
                         BotCommand("shuffle", "Rᴀɴᴅᴏᴍʟʏ sʜᴜғғʟᴇs ᴛʜᴇ ǫᴜᴇᴜᴇᴅ ᴘʟᴀʏʟɪsᴛ."),
-                        BotCommand(
-                            "playmode",
-                            "Aʟʟᴏᴡs ʏᴏᴜ ᴛᴏ ᴄʜᴀɴɢᴇ ᴛʜᴇ ᴅᴇғᴀᴜʟᴛ ᴘʟᴀʏᴍᴏᴅᴇ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ",
-                        ),
-                        BotCommand(
-                            "settings",
-                            "Oᴘᴇɴ ᴛʜᴇ sᴇᴛᴛɪɴɢs ᴏғ ᴛʜᴇ ᴍᴜsɪᴄ ʙᴏᴛ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ.",
-                        ),
+                        BotCommand("playmode", "Aʟʟᴏᴡs ʏᴏᴜ ᴛᴏ ᴄʜᴀɴɢᴇ ᴛʜᴇ ᴅᴇғᴀᴜʟᴛ ᴘʟᴀʏᴍᴏᴅᴇ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ"),
+                        BotCommand("settings", "Oᴘᴇɴ ᴛʜᴇ sᴇᴛᴛɪɴɢs ᴏғ ᴛʜᴇ ᴍᴜsɪᴄ ʙᴏᴛ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ."),
                     ]
                 )
-            except:
-                pass
-        else:
-            pass
+            except Exception as e:
+                LOGGER(__name__).error(f"Failed to set bot commands. Error: {str(e)}")
+
         try:
             a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
             if a.status != ChatMemberStatus.ADMINISTRATOR:
                 LOGGER(__name__).error("Please promote Bot as Admin in Logger Group")
                 sys.exit()
-        except Exception:
-            pass
-        if get_me.last_name:
-            self.name = get_me.first_name + " " + get_me.last_name
-        else:
-            self.name = get_me.first_name
+        except Exception as e:
+            LOGGER(__name__).error(f"Failed to check admin status in log group. Error: {str(e)}")
+
         LOGGER(__name__).info(f"MusicBot Started as {self.name}")
 
-
+        # Start app2
         if config.BOT_TOKEN2:
             await self.two.start()
-            get_me = await self.two.get_me()
-            self.one.username = get_me.username
-            self.one.id = get_me.id
-            self.one.name = get_me.first_name + " " + (get_me.last_name or "")
-            self.one.mention = get_me.mention
+            get_me_app2 = await self.two.get_me()
+            self.two.username = get_me_app2.username
+            self.two.id = get_me_app2.id
+            self.two.name = get_me_app2.first_name + " " + (get_me_app2.last_name or "")
+            self.two.mention = get_me_app2.mention
 
             try:
-                await self.one.send_message(
+                await self.two.send_message(
                     config.LOG_GROUP_ID,
-                    text=f"<u><b>{self.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.id}</code>\nɴᴀᴍᴇ : {self.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                    text=f"<u><b>{self.two.mention} ʙᴏᴛ sᴛᴀʀᴛᴇᴅ :</b><u>\n\nɪᴅ : <code>{self.two.id}</code>\nɴᴀᴍᴇ : {self.two.name}\nᴜsᴇʀɴᴀᴍᴇ : @{self.two.username}",
                 )
-            except:
+            except Exception as e:
                 LOGGER(__name__).error(
-                "Bot2 has failed to access the log Group. Make sure that you have added your bot to your log channel and promoted as admin!"
+                    f"Bot2 has failed to access the log Group. Error: {str(e)}"
                 )
                 sys.exit()
+
             if config.SET_CMDS:
                 try:
-                    await self.set_bot_commands(
-                    [
-                        BotCommand("help", "ɢᴇᴛ ᴛʜᴇ ʜᴇʟᴘ ᴍᴇɴᴜ"),
-                        BotCommand("ping", "ᴄʜᴇᴄᴋ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ"),
-                        BotCommand("play", "sᴛᴀʀᴛ ᴘʟᴀʏɪɴɢ ʀᴇǫᴜᴇᴛᴇᴅ sᴏɴɢ"),
-                        BotCommand("skip", "ᴍᴏᴠᴇ ᴛᴏ ɴᴇxᴛ ᴛʀᴀᴄᴋ ɪɴ ǫᴜᴇᴜᴇ"),
-                        BotCommand("pause", "ᴘʟᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴘʟᴀʏɪɴɢ sᴏɴɢ"),
-                        BotCommand("resume", "ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴘᴀᴜsᴇᴅ sᴏɴɢ"),
+                    await self.app2.set_bot_commands(
+                        [
+                            BotCommand("help", "ɢᴇᴛ ᴛʜᴇ ʜᴇʟᴘ ᴍᴇɴᴜ"),
+                            BotCommand("ping", "ᴄʜᴇᴄᴋ ʙᴏᴛ ɪs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ"),
+                            BotCommand("play", "sᴛᴀʀᴛ ᴘʟᴀʏɪɴɢ ʀᴇǫᴜᴇᴛᴇᴅ sᴏɴɢ"),
+                            BotCommand("skip", "ᴍᴏᴠᴇ ᴛᴏ ɴᴇxᴛ ᴛʀᴀᴄᴋ ɪɴ ǫᴜᴇᴜᴇ"),
+                            BotCommand("pause", "ᴘʟᴀᴜsᴇ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴘʟᴀʏɪɴɢ sᴏɴɢ"),
+                            BotCommand("resume", "ʀᴇsᴜᴍᴇ ᴛʜᴇ ᴘᴀᴜsᴇᴅ sᴏɴɢ"),
                         BotCommand("end", "ᴄʟᴇᴀʀ ᴛʜᴇ ǫᴜᴇᴜᴇ ᴀᴍᴅ ʟᴇᴀᴠᴇ ᴠᴏɪᴄᴇᴄʜᴀᴛ"),
-                        BotCommand("shuffle", "Rᴀɴᴅᴏᴍʟʏ sʜᴜғғʟᴇs ᴛʜᴇ ǫᴜᴇᴜᴇᴅ ᴘʟᴀʏʟɪsᴛ."),
-                        BotCommand(
-                            "playmode",
-                            "Aʟʟᴏᴡs ʏᴏᴜ ᴛᴏ ᴄʜᴀɴɢᴇ ᴛʜᴇ ᴅᴇғᴀᴜʟᴛ ᴘʟᴀʏᴍᴏᴅᴇ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ",
-                        ),
-                        BotCommand(
-                            "settings",
-                            "Oᴘᴇɴ ᴛʜᴇ sᴇᴛᴛɪɴɢs ᴏғ ᴛʜᴇ ᴍᴜsɪᴄ ʙᴏᴛ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ.",
-                        ),
+                            BotCommand("shuffle", "Rᴀɴᴅᴏᴍʟʏ sʜᴜғғʟᴇs ᴛʜᴇ ǫᴜᴇᴜᴇᴅ ᴘʟᴀʏʟɪsᴛ."),
+                            BotCommand("playmode", "Aʟʟᴏᴡs ʏᴏᴜ ᴛᴏ ᴄʜᴀɴɢᴇ ᴛʜᴇ ᴅᴇғᴀᴜʟᴛ ᴘʟᴀʏᴍᴏᴅᴇ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ"),
+                            BotCommand("settings", "Oᴘᴇɴ ᴛʜᴇ sᴇᴛᴛɪɴɢs ᴏғ ᴛʜᴇ ᴍᴜsɪᴄ ʙᴏᴛ ғᴏʀ ʏᴏᴜʀ ᴄʜᴀᴛ."),
                         ]
                     )
-                except:
+                except Exception:
                     pass
-            else:
-                pass
+
             try:
-                a = await self.get_chat_member(config.LOG_GROUP_ID, self.id)
-                if a.status != ChatMemberStatus.ADMINISTRATOR:
-                     LOGGER(__name__).error("Please promote Bot2 as Admin in Logger Group")
+                b = await self.two.get_chat_member(config.LOG_GROUP_ID, self.two.id)
+                if b.status != ChatMemberStatus.ADMINISTRATOR:
+                    LOGGER(__name__).error("Please promote Bot2 as Admin in Logger Group")
                     sys.exit()
             except Exception:
                 pass
-            if get_me.last_name:
-                self.one.name = get_me.first_name + " " + get_me.last_name
-            else:
-                self.one.name = get_me.first_name
-            LOGGER(__name__).info(f"MusicBot Started as {self.one.name}")
+
+        LOGGER(__name__).info(f"MusicBot2 Started as {self.two.name}")
